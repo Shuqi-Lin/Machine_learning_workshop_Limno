3180453f889174f0f1684a3f89988d89718629e8
Shuqi Lin <85072357+Shuqi-Lin@users.noreply.github.com>
2022-06-17 15:58:48 +0200

    Hand-on exercise

--- Random Forest exercise.py
+++ Random Forest exercise.py
@@ -108,3 +108,92 @@ Pred.plot(style='ro',alpha=0.5,ax=ax)
 df.plot(x='Date',y='Chl',style='go',ax=ax)
 ax.set_xlim([pd.Timestamp(2004,1,1),pd.Timestamp(2008,1,1)])
 ax.legend(['Prediction','Observation'],frameon=False)
+
+#%%Load training dataset (for classification question)
+# Make sure you are in the main folder('..\Machine_learning_workshop')
+df=pd.read_csv('Classification dataset.csv',sep='\t',parse_dates=['Date'])
+
+#%% Browse your dataset and create basic data visualizations
+df.info()
+fig,ax=plt.subplots(figsize = (12,2))
+df[df['Bloom']==1].plot(x='Date',y='Bloom',style='rs',markersize=20,alpha=0.3,ax=ax)
+ax.set_xlim([pd.Timestamp(2004,1,1),pd.Timestamp(2008,1,1)])
+ax.legend('')
+## Turn the target 'Bloom' column into categorical data
+df['Bloom']=pd.Categorical(df['Bloom'])
+## Check the number of days counted as algal bloom date
+df['Bloom'].value_counts()
+## 0 represents no bloom
+## 1 represents bloom
+
+#%% Start to built the RF classifier
+# Choose the features and target, and make sure they are in the same length
+X=df[['SST', 'U', 'AirT', 'Humidity', 'CC', 'Prec', 'SWR', 'inflow',
+       'thermD', 'MLD', 'W']]
+y=df[['Bloom']].values
+print('Shape of feature dataframe: {}'.format(X.shape))
+print('Length of target column {}'.format(len(y)))
+# Split the training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+print('Shape of training dataset: {}'.format(X_train.shape))
+print('Shape of testing dataset: {}'.format(X_test.shape))
+
+#%% Since the classes in 'Bloom' are unbalanced, the class weight should be specified before training the model
+RF_classifier=RandomForestClassifier(class_weight="balanced")
+## When tuning the hyperparameters, the scoring method should be specified in terms of unbalanced classes
+RSgrid = RandomizedSearchCV(estimator = RF_classifier,param_distributions = param_grid,
+                            scoring='balanced_accuracy',n_iter = 10,cv = 10,verbose = 1,
+                            random_state=101)
+
+RSgrid.fit(X_train, y_train)
+RF_classifier=RandomForestClassifier(class_weight="balanced").set_params(**RSgrid.best_params_)
+
+#%% Train the model with the optimal hyperparameter combination
+RF_classifier.fit(X_train, y_train)
+print('R2 in training dataset: {}'.format(round(RF_classifier.score(X_train,y_train),2))) 
+print('R2 in testing dataset: {}'.format(round(RF_classifier.score(X_test,y_test),2))) 
+
+#%% Additional function in RF classifier: it gives predict class probabilities
+Prob=pd.DataFrame(RF_classifier.predict_proba(X_test),columns=[0,1])
+Prob['Pred']=RF_classifier.predict(X_test)
+Prob.head(30)
+
+#%% Practical Use: Assume we only have data in 1999-2018 and want to predict the algal bloom date in 2019-2020 based on the available environmental factors
+train_df=df[df['Date']<pd.Timestamp(2019,1,1)]
+test_df=df[df['Date']>=pd.Timestamp(2019,1,1)]
+X_train=train_df[['SST', 'U', 'AirT', 'Humidity', 'CC', 'Prec', 'SWR', 'inflow','thermD', 'MLD', 'W']]
+y_train=train_df['Bloom']
+RF_classifier=RandomForestClassifier(class_weight="balanced")
+## When tuning the hyperparameters, the scoring method should be specified in terms of unbalanced classes
+RSgrid = RandomizedSearchCV(estimator = RF_classifier,param_distributions = param_grid,
+                            scoring='balanced_accuracy',n_iter = 10,cv = 10,verbose = 1,
+                            random_state=101)
+
+RSgrid.fit(X_train, y_train)
+RF_classifier=RandomForestClassifier(class_weight="balanced").set_params(**RSgrid.best_params_)
+RF_classifier.fit(X_train, y_train)
+print('R2 in training dataset: {}'.format(round(RF_classifier.score(X_train,y_train),2))) 
+#%% Apply the model to testing dataset
+X_test=test_df[['SST', 'U', 'AirT', 'Humidity', 'CC', 'Prec', 'SWR', 'inflow','thermD', 'MLD', 'W']]
+y_test=test_df['Bloom'].values
+Prob=pd.DataFrame(RF_classifier.predict_proba(X_test),columns=[0,1],index=test_df['Date'])
+Prob['Pred']=RF_classifier.predict(X_test)
+Prob['True']=y_test
+Prob['True']=Prob['True'].astype('int64')
+print('R2 in testing dataset: {}'.format(round(RF_classifier.score(X_test,y_test),2))) 
+
+fig,ax=plt.subplots(figsize = (12,2))
+Prob[Prob['Pred']==1].plot(y='Pred',style='bs',markersize=15,alpha=0.3,ax=ax,label='Predicted bloom')
+(Prob[Prob['True']==1]+0.3).plot(y='True',style='rs',markersize=15,alpha=0.3,ax=ax,label='Observed bloom')
+ax.set_xlim([pd.Timestamp(2019,1,1),pd.Timestamp(2021,1,1)])
+ax.set_ylim([0.9,1.5])
+ax.legend(ncol=2,frameon=False)
+
+#%% Feature importance
+importances = RF_classifier.feature_importances_
+forest_importances = pd.Series(importances, index=X_test.columns).sort_values(ascending=True)
+fig, ax = plt.subplots(figsize=(6,6))
+forest_importances.plot.barh(ax=ax)
+ax.set_title("Feature importances using mean decrease in impurity")
+ax.set_ylabel("Mean decrease in impurity")
+fig.tight_layout()
